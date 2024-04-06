23ba4cc44990b369ae6f9effe26503cd078c8260
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-06 20:50:13 +0200

    string reverser v1

---
+++ ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReverseString;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome in your string reverser!");
+        string inputString = "";
+        do
+        {
+            Console.Write("String to reverse : ");
+            inputString = Console.ReadLine();
+        } while (inputString.Trim() == "");
+
+
+        string reversedString = "";
+        for (int i = inputString.Length - 1; i >= 0; i--)
+        {
+            reversedString += inputString[i];
+        }
+
+        Console.WriteLine($"The reversed string is : {reversedString}");
+
+        if (reversedString.ToLower() == inputString.ToLower())
+        {
+            Console.WriteLine("Your word is a palindrome!");
+        }
+    }
+}
---
+++ ReverseString/README.md
@@ -0,0 +1,8 @@
+# ReverseString
+
+Create a program that asks the user for a string of input and simply returns it in reverse order. For instance, they enter “Hello” and it returns “olleH”.
+
+Added Difficulty: Check if the given string is a palindrome.
+(a palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or racecar.)
+
+rocket Tips: If you have problems understanding loops in general you could take a look at Module 3 regarding the debugger
